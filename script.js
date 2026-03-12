58cf4c345bea67bc24bc1d6ea92cd1d3e02e1157
maycon_v_f <maycon.fedechen@escola.pr.gov.br>
2026-03-12 15:22:34 +0000

    usando variavel

---
+++ script.js
@@ -0,0 +1,20 @@
+// var
+
+//var altura =5;
+//var comprimento =7;
+
+//area = altura * comprimento;
+//console.log(area);
+
+
+let  forma = 'retângulo';
+let altura = 5;
+let comprimento =7;
+
+if (forma === 'retangulo'){
+area = altura * comprimento;
+} else{
+   area = (altura * comprimento) / 2;
+
+
+console.log(area);
